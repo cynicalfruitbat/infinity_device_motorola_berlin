691efb2339b8f5f258cdc0d9a2347e80fcd08f67
cynicalfruitbat <ethanodonnell10@gmail.com>
2025-12-01 17:11:22 +0000

    Removed duplicate system.prop; already exists in sm73250-common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/a94000.i2c/i2c-2/2-0049/do
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
